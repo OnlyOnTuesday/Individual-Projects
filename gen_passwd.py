2b03538f4dd753cbbbc936b105b8ffa3c0a25d0b
Michael <mikeycooney12@gmail.com>
2018-01-03 21:34:25 -0500

    Added functionality to gen_passwd.py, ruined its output.  Will be fixed later

--- gen_passwd.py
+++ gen_passwd.py
@@ -8,8 +8,10 @@
 ###################################
 
 
-from random import choice
+from random import choice, randint
+import sys
 
+terminal_args = list(sys.argv)
 
 # Colors will be oraganized in the following order:
 # the primaries, their basic mixtures along with white, black, grey, and brown.
@@ -37,12 +39,88 @@ ends = ["AreAttractive", "AreBald", "LikeCleanliness", "AreDazzling", "AreDrab",
               "CoachBaseball", "PilotJets", "AdviseLeaders", "MentorActors", "EmployDirtyTricks", \
               "PopulateQuickly", "ElectDictators", "ConfrontDeath", "TriangulateCircles"]
 
+# Add special character support.
+special_chars = ["!", "@", "#", "$", "%", "^", "&", "*", " ", "+", "=", "-", "_", ",", ".", "~", \
+                 "{", "}", "[", "]", "(", ")"]
+
+# add number support.  This will either add a series of numbers to the beginning or end of password,
+# or it will replace certain characters with numbers (such as "o" with "0").  The choice will be
+# made by the program based on a random outcome.
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+#allows us to find name of calling function
+getframe_expr = "sys._getframe({}).f_code.co_name"
+
+def use_numbers(list):
+    if "-n" in terminal_args:
+        #choose 1 or 2; this will determine if the numbers replace letters or are interspersed
+        num = randint(1, 2)
+        if num == 1:
+            #interspersed
+            return 1
+        else:
+            #replace letters
+            return 2
+    else:
+        return False
+            
+
+def interspersed(passwd):
+    #will numbers appear at beginning/end or inbetween words, or will they replace letters
+    num = randint(1,2)
+    if num == 1:
+        #inbetween words
+        num2 = randint(1, 5)
+        passwd = passwd.split()
+        for i in range(1, num2):
+            if any (char in "ABCDEFGHIJKLMNOPQRSTUVWXYV1234567890" for char in passwd):
+                passwd.insert(char, randint(1,10))
+                return passwd
+    elif num == 2:
+        #before or after the password
+        num2 = randint(1,2)
+        if num2 == 1:
+            #before word
+            passwd = passwd.split()
+            num3 = randint(1,5)
+            for i in range(1, num3):
+                passwd.insert(0, randint(1,10))
+                return passwd
+        elif num2 == 2:
+            #after word
+            passwd = passwd.split()
+            length = len(passwd)
+            num3 = randint(1,5)
+            for i in range(1, num3):
+                passwd.insert(length, randint(1,10)) 
+                return passwd
+
+def replace_letters(passwd):
+    #replace letters inside of words.
+    #requires certain letters to be paired with certain numbers
+    passwd = list(passwd)
+    for char in passwd:
+        if char == "o" or char == "O":
+            passwd[passwd.index(char)] = "0"
+        elif char == "i" or char == "I":
+            passwd[passwd.index(char)] = "1"
+    return passwd
+            
 def get_passwd(color, animal, end):
+    numbers = use_numbers(terminal_args)
     color_choice = choice(color)
     animal_choice = choice(animal)
     end_choice = choice(end)
-    print(color_choice + animal_choice + end_choice)
-
-
-for i in range(5):
-    get_passwd(colors, animals, ends)
+    passwd = color_choice + animal_choice + end_choice
+    #print(color_choice + animal_choice + end_choice)
+    if numbers == 1:
+        new_passwd = interspersed(passwd)
+        print(new_passwd)
+    elif numbers == 2:
+        new_passwd = replace_letters(passwd)
+        print(new_passwd)
+    else:
+        print(passwd)
+        
+for i in range(1, 10):
+        get_passwd(colors, animals, ends)
